80737e14c5864f82aaeb8c6c2cd82512edbf3519
Alex Emelyanov <holyketzer@gmail.com>
2019-05-04 07:27:07 +0300

    Disable monitoring and error reporiting gems in specs

--- lib/instrumentation.rb
+++ lib/instrumentation.rb
@@ -1,22 +1,33 @@
 module Instrumentation
-  def add_param_context(*keys)
-    keys.each do |key|
-      honeycomb_metadata[key] = params[key]
+  if Rails.env.test?
+    def add_param_context(*keys)
     end
-  end
 
-  def add_context(metadata)
-    metadata.each do |key, value|
-      honeycomb_metadata[key] = value
+    def add_context(metadata)
     end
-  end
 
-  def append_to_honeycomb(request, controller_name)
-    return if honeycomb_metadata.nil?
+    def append_to_honeycomb(request, controller_name)
+    end
+  else
+    def add_param_context(*keys)
+      keys.each do |key|
+        honeycomb_metadata[key] = params[key]
+      end
+    end
 
-    honeycomb_metadata["trace.trace_id"] = request.request_id
-    honeycomb_metadata["trace.span_id"] = request.request_id
-    honeycomb_metadata[:service_name] = "rails"
-    honeycomb_metadata[:name] = controller_name
+    def add_context(metadata)
+      metadata.each do |key, value|
+        honeycomb_metadata[key] = value
+      end
+    end
+
+    def append_to_honeycomb(request, controller_name)
+      return if honeycomb_metadata.nil?
+
+      honeycomb_metadata["trace.trace_id"] = request.request_id
+      honeycomb_metadata["trace.span_id"] = request.request_id
+      honeycomb_metadata[:service_name] = "rails"
+      honeycomb_metadata[:name] = controller_name
+    end
   end
 end
